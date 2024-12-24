0f8d30c3ed48fe225c27bf8b961c702ec0087448
Cameron Pak <cameronandrewpak@gmail.com>
2024-12-23 21:34:02 -0600

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+import netlify from "@astrojs/netlify";
+import db from "@astrojs/db";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    db(),
+    tailwind(),
+    alpinejs({
+      entrypoint: "/src/entrypoint"
+    })
+  ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["astro:db"]
+    }
+  },
+  output: "server",
+  adapter: netlify(),
+  experimental: {
+    serverIslands: true
+  }
+});
